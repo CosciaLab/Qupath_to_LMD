05a4f56c696c90d9fc5b30701279d8fe06f409bc
Jose Nimo <nimojose@gmail.com>
2023-06-07 17:52:01 +0200

    updated to have box for samples and wells

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,47 +6,27 @@ import tifffile
 import shapely
 import streamlit as st
 from python_functions import dataframe_to_xml_v2
-#
-from PIL import Image
-from pathlib import Path
-
-import subprocess
-import sys
-import time
-
-
-try:
-  # replace "yourpackage" with the package you want to import
-   from lmd.lib import Collection, Shape
-
-# This block executes only on the first run when your package isn't installed
-except ModuleNotFoundError as e:
-  subprocess.Popen([f'{sys.executable} -m pip install git+https://github.com/MannLabs/py-lmd.git'], shell=True)
-  # wait for subprocess to install package before running your actual code below
-  time.sleep(90)
-
 from lmd.lib import SegmentationLoader
 from lmd.lib import Collection, Shape
 from lmd import tools
-"""
-# Welcome to Streamlit!
-
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
-
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
-
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+from PIL import Image
+from pathlib import Path
 
 st.title("Convert a GeoJSON polygons to xml")
+st.subheader("From Jose Nimo, at AG Coscia in the Max Delbrueck Center for Molecular Medicine in Berlin")
+st.subheader("Thanks to Florian Wuenemman for inspiration, and Sophia Madler for the lmd-py package")
+st.subheader("Contact jose.nimo@mdc-berlin.de for questions and suggestions")
 
 uploaded_file = st.file_uploader("Choose a file", accept_multiple_files=False)
 
-calibration_point_1 = st.text_input("Enter the name of the first calibration point: ")
-calibration_point_2 = st.text_input("Enter the name of the second calibration point: ")
-calibration_point_3 = st.text_input("Enter the name of the third calibration point: ")
+calibration_point_1 = st.text_input("Enter the name of the first calibration point: ",  placeholder ="calib1")
+calibration_point_2 = st.text_input("Enter the name of the second calibration point: ", placeholder ="calib2")
+calibration_point_3 = st.text_input("Enter the name of the third calibration point: ",  placeholder ="calib3")
 
+samples_and_wells_input = st.text_area("Enter the desired samples and wells scheme")
+
+import ast
+samples_and_wells = ast.literal_eval(samples_and_wells_input)
 
 if st.button("Process file"):
    calibration_points = [calibration_point_1, calibration_point_2, calibration_point_3]
