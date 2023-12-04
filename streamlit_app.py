fe14a6fddde7c6e504999e5bf168af2bd8601e18
josenimo <nimojose@gmail.com>
2023-12-04 14:48:25 +0100

    alpha run

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@ import geopandas
 import pandas
 import numpy
 import datetime
+datetime = dt.datetime.today().strftime("%Y%m%d_%H%M%S")
 import tifffile
 import shapely
 import streamlit as st
@@ -14,44 +15,150 @@ from pathlib import Path
 import ast
 
 
-
 st.title("Convert a GeoJSON polygons to xml for Laser Microdissection")
 st.subheader("From Jose Nimo, PhD at AG Coscia in the Max Delbrueck Center for Molecular Medicine in Berlin")
 st.divider()
 
 uploaded_file = st.file_uploader("Choose a file", accept_multiple_files=False)
 
-#button to print information about the file, from a function
-if st.button("Print information about the file"):
-   if uploaded_file is not None:
-      
+@st.cache(allow_output_mutation=True)
+def load_and_QC_geojson_file(geojson_path: str, list_of_calibpoint_names: list = ['calib1','calib2','calib3']):
 
+    #load geojson file
+    df = geopandas.read_file(path_to_file)
 
+    #save calib points in a list
+    caliblist = []
+    for point_name in list_of_calibpoint_names:
+        if point_name in df['name'].unique():
+            caliblist.append(df.loc[df['name'] == point_name, 'geometry'].values[0])
+        else:
+            st.write('Your given name is not present in the file', 
+            f'These are the calib points you passed: {list_of_calibpoint_names}',
+            f"These are the calib points found in the geojson you gave me: {df['name'].unique()}")
+    #create coordenate list
+    listarray = []
+    for point in pointlist:
+        listarray.append([point.x, point.y])
+    calib_np_array = np.array(listarray)
+
+    #now that calibration points are saved, remove them from the dataframe
+    df = df[df['name'].isin(list_of_calibpoint_names) == False]
+
+    #check and remove empty classifications 
+    if df['classification'].isna().sum() !=0 :
+        st.write(f"you have {df['classification'].isna().sum()} NaNs in your classification column",
+                "these are unclassified objects from Qupath, they will be ignored") 
+        df = df[df['classification'].notna()]
+
+    #check for MultiPolygon objects
+    if 'MultiPolygon' in df.geometry.geom_type.value_counts().keys():
+        st.write('MultiPolygon objects present:',
+        #print out the classification name of the MultiPolygon objects
+        f"{df[df.geometry.geom_type == 'MultiPolygon']['classification']}", 
+        'these are not supported, please convert them to polygons in Qupath',
+        'the script will continue but these objects will be ignored')
+        #remove MultiPolygon objects
+        df = df[df.geometry.geom_type != 'MultiPolygon']
+
+    # reformat shape coordenate list
+    df['coords'] = np.nan
+    df['coords'] = df['coords'].astype('object')
+    # simplify to reduce number of points
+    df['simple'] = df.geometry.simplify(1)
+    for i in df.index:
+        geom=df.at[i, 'simple']
+        tmp = list(geom.exterior.coords)
+        tmp_lol = [list(i) for i in tmp]
+        df.at[i,'coords'] = tmp_lol
+
+    #extract classification name into a new column
+    df['Name'] = np.nan
+    for i in df.index:
+        tmp = df.classification[i].get('name')
+        df.at[i,'Name'] = tmp
+
+    st.write('The file loading is complete')
+
+    return(df,calib_np_array)
 
 calibration_point_1 = st.text_input("Enter the name of the first calibration point: ",  placeholder ="calib1")
 calibration_point_2 = st.text_input("Enter the name of the second calibration point: ", placeholder ="calib2")
 calibration_point_3 = st.text_input("Enter the name of the third calibration point: ",  placeholder ="calib3")
+list_of_calibpoint_names = [calibration_point_1, calibration_point_2, calibration_point_3]
+
+if st.button("Load and check the geojson file"):
+    df, calib_np_array = load_and_QC_geojson_file(uploaded_file, list_of_calibpoint_names)
 
 samples_and_wells_input = st.text_area("Enter the desired samples and wells scheme, this is required!")
 
 @st.cache(allow_output_mutation=True)
-def run_script(uploaded_file, calibration_points, samples_and_wells):
-   # Add your script logic here
-   dataframe_to_xml_v2(uploaded_file, calibration_points, samples_and_wells)
+def load_and_QC_SamplesandWells(samples_and_wells_input, df):
 
-if st.button("Run the script"):
-   #load samples and wells
-   samples_and_wells_processed = samples_and_wells_input.replace("\n", "")
-   samples_and_wells_processed = samples_and_wells_processed.replace(" ", "")
-   samples_and_wells = ast.literal_eval(samples_and_wells_processed)
-   #load calibration points
-   calibration_points = [calibration_point_1, calibration_point_2, calibration_point_3]
-   st.write("these are your calibration points: ")
-   st.write(calibration_points)
-   # Run your script or process the inputs
-   st.write("Running the script...")
-   # Add your script logic here
-   run_script(uploaded_file, calibration_points, samples_and_wells)
-   #Running is done
-   st.write("Please download the file now")
-   st.download_button("Download file", Path("./out.xml").read_text(), "out.xml")
+    # parse common human copy paste formats
+    # remove newlines
+    samples_and_wells_processed = samples_and_wells_input.replace("\n", "")
+    # remove spaces
+    samples_and_wells_processed = samples_and_wells_processed.replace(" ", "")
+    #parse into python dictionary
+    samples_and_wells = ast.literal_eval(samples_and_wells_processed)
+
+    #create list of acceptable wells, default is using a space in between columns
+    list_of_acceptable_wells =[]
+    for row in list(string.ascii_uppercase[2:14]):
+        for column in range(2,22):
+            list_of_acceptable_wells.append(str(row) + str(column))
+
+    #check for improper wells
+    for well in samples_and_wells.values():
+        if well not in list_of_acceptable_wells:
+            st.write(f'Your well {well} is not in the list of acceptable wells, please correct it',
+            'the LMD is not able to collect into this well, the script will stop here')
+            st.stop()
+
+    #check that names in df are all present in the samples and wells
+    for name in df.Name.unique():
+        if name not in samples_and_wells.keys():
+            st.write(f'Your name {name} is not in the list of samples_and_wells, please correct either',
+            'please change the class name in Qupath or add it to the samples_and_wells dictionary',
+            'and then rerun the web app')
+            st.stop()
+    
+    return samples_and_wells
+
+if st.button("Check the samples and wells"):
+    samples_and_wells = load_and_QC_SamplesandWells(samples_and_wells_input, df)
+
+
+
+@st.cache(allow_output_mutation=True)
+def create_collection(df = df, calib_np_array = calib_np_array, samples_and_wells = samples_and_wells ):
+    #create the collection of py-lmd-env package
+    #uses caliblist passed on the function, order matters
+    #orientation vector is for QuPath coordenate system
+    the_collection = Collection(calibration_points = calib_np_array)
+    the_collection.orientation_transform = np.array([[1,0 ], [0,-1]])
+    for i in df.index:
+        the_collection.new_shape(df.at[i,'coords'], well = samples_and_wells[df.at[i, "Name"]])
+
+    the_collection.plot(save_name= "./TheCollection.png")
+    st.image("./TheCollection.png", caption='Your Contours', use_column_width=True)
+    st.write(the_collection.stats())
+    the_collection.save(f"./{datetime}_LMD_ready_contours.xml")
+    
+
+    #create and export dataframe with sample placement in 384 well plate
+    rows_A_P= [i for i in string.ascii_uppercase[:16]]
+    columns_1_24 = [str(i) for i in range(1,25)]
+    df_wp384 = pd.DataFrame('',columns=columns_1_24, index=rows_A_P)
+    #fill in the dataframe with samples and wells
+    for i in samples_and_wells:
+        location = samples_and_wells[i]
+        df_wp384.at[location[0],location[1:]] = i
+    #save dataframe as csv
+    df_wp384.to_csv(f"./{datetime}_384_wellplate.csv", index=True)
+
+if st.button("Process geojson and create the contours"):
+    create_collection(df, calib_np_array, samples_and_wells)
+    st.download_button("Download contours file", Path(f"./{datetime}_LMD_ready_contours.xml").read_text(), f"./{datetime}_LMD_ready_contours.xml")
+    st.download_button("Download 384 plate scheme", Path(f"./{datetime}_384_wellplate.csv").read_text(), f"./{datetime}_384_wellplate.csv")
