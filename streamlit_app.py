9b848c526be39bb684947cfecd0f4b1aa54b03fc
Jose Nimo <nimojose@gmail.com>
2024-12-05 16:11:53 +0100

    message about upper limit geojson

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,7 +19,7 @@ logger.add(sys.stdout, format="<green>{time:HH:mm:ss.SS}</green> | <level>{level
 
 st.title("Convert a GeoJSON polygons to xml for Laser Microdissection")
 st.subheader("From Jose Nimo, PhD at AG Coscia in the Max Delbrueck Center for Molecular Medicine in Berlin")
-st.subheader("I am aware of resource issues, as of 04.12.2024, please report github issue if you encounter any")
+st.subheader("If geojson abovve 150MB there might be resource issues, I suggest running locally, see tutorial in github")
 st.divider()
 
 uploaded_file = st.file_uploader("Choose a file", accept_multiple_files=False)
